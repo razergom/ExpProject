21a586455dcbccef98a457fa35a8f3569f97a720
Andrey Petrov <thenolldd@gmail.com>
2020-06-25 21:39:15 +0300

    index.js to index.jsx

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   entry: {
-    app: "./src/index.js",
+    app: "./src/index.jsx",
   },
   devServer: {
     contentBase: "./dist",
